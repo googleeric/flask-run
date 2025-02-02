05db1cd4214f8f1bff4ccdb148508f9b9da1b317
googleeric <eric.eagle938@gmail.com>
2025-02-02 21:40:09 +0530

    new updated index

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Ensure Flask uses the correct port
-    app.run(host="0.0.0.0", port=port)
+
+@app.route('/')
+def index():
+
+    return  render_template("index.html")
+
