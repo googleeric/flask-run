2b3987f69fc8d15eb03c8add62e36c0310a07bfa
googleeric <eric.eagle938@gmail.com>
2025-02-02 21:22:19 +0530

    app update2

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, jsonify, request
-
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-
-
-@app.route('/')
+@app.route("/")
 def index():
+    return "Hello World"
 
-    return  "Hello World"
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Ensure Flask uses the correct port
+    app.run(host="0.0.0.0", port=port)
